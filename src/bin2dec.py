2381bfcc8c1baf2bebdbc63489bd52bfab67a92a
Jhonatan-TRABALHO <edisonjhonatan@gmail.com>
2022-04-07 17:41:13 -0300

    Implemented Bin2Dec class. All tests passing (GREEN TDD step)

--- src/bin2dec.py
+++ src/bin2dec.py
@@ -1,1 +1,32 @@
 
+
+class Bin2Dec:
+    
+    def __init__(self, binary_number):
+        self.binary_number = binary_number
+        
+        self.check_invalid_digits()
+        self.check_eight_digit_limit()
+        self.convert_binary_to_decimal()
+        
+    def get_number(self):
+        return self.binary_number
+    
+    def check_invalid_digits(self):
+        number = str(self.get_number())
+        
+        for caracter in number:
+            print(caracter)
+            if caracter != "0" and caracter != "1":
+                raise ValueError
+            
+    def check_eight_digit_limit(self):
+        number_of_digits = len(str(self.get_number()))
+        
+        if number_of_digits > 8:
+            raise ValueError
+        
+    def convert_binary_to_decimal(self):
+        binary_number = str(self.get_number())
+        return int(binary_number, 2)
+    
--- tests/test_bin2dec.py
+++ tests/test_bin2dec.py
@@ -1,4 +1,5 @@
 import pytest
+from src.bin2dec import Bin2Dec
 
 def test_create_object_of_type_bin2_dec():
 	converter = Bin2Dec(101)
@@ -10,22 +11,21 @@ def test_get_number():
 
 	assert converter.get_number() == 101
 
-
 def test_input_with_invalid_characters():
     with pytest.raises(ValueError):
         
-        converter = Bin2Dec(abcde)
-
-def test_input_cannot_be_more_than_eight_digits():
-    with pytest.raises(ValueError):
-        
-        converter = Bin2Dec(101101101)
+        converter = Bin2Dec('abcde')
 
 def test_input_must_allow_only_binary_number():
     with pytest.raises(ValueError):
         
         converter = Bin2Dec(12345)
+
+def test_input_cannot_be_more_than_eight_digits():
+    with pytest.raises(ValueError):
         
+        converter = Bin2Dec(111100001111)
+
 def test_convert_binary_to_decimal():
     converter = Bin2Dec(101)
     
