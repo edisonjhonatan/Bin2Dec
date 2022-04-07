f5ebe5682150b949dfcc35f192fd2dc2137e144e
Jhonatan-TRABALHO <edisonjhonatan@gmail.com>
2022-04-07 16:50:19 -0300

    Writing test methods signatures

---
+++ tests/test_bin2dec.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_create_object_of_type_bin2_dec():
+
+def test_get_number():
+
+def test_input_with_invalid_characters():
+
+def test_input_cannot_be_more_than_eight_digits():
+
+def test_input_must_allow_only_binary_number():
+
+def test_convert_binary_to_decimal():
